df8ee6eda8ac3d61a8df3859260051f3864d3a5d
a96363877 <a96363877@gmail.com>
2025-01-12 18:16:35 -0800

    Update cards.tsx

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, DocumentData, DocumentReference  } from 'firebase/firestore'
 import { db } from '@/lib/firestore'
 import Image from 'next/image'
 
