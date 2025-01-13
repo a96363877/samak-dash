3af5ec4736c07b97930f5717c3694db9d1435afa
a96363877 <a96363877@gmail.com>
2025-01-12 23:15:29 -0800

    Update page.tsx

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -258,13 +258,8 @@ export default function NotificationsPage() {
                   <td className="px-4 py-3">
                     <div className="flex flex-col sm:flex-row gap-2 justify-center">
                       <Badge
-                        variant={
-                          user.data
-                            ? cardData.some((card) => card.id === user.id)
-                              ? "secondary"
-                              : "default"
-                            : "destructive"
-                        }
+                                             variant={user.data ? (cardData.some(card => card.id === user.id) ? 'secondary' : 'default') : 'destructive'}
+
                         className="rounded-md cursor-pointer"
                         onClick={() => handleInfoClick(user, "personal")}
                       >
